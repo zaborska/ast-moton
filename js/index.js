1b0a3ececdc29e2bd0d39478bbc9a5f928ac275b
Yulia Zaborska <yulia@MacBook-Pro-Yulia.local>
2019-09-03 20:04:35 +0300

    accordion

--- js/index.js
+++ js/index.js
@@ -5,43 +5,9 @@ burgerBtn.addEventListener("click", () => {
   mobile.classList.toggle("navigation");
 });
 
-// FOR DEVELOPMENT PURPOSES - INCLUDE PARTS OF HTML
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("include-html");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-            elmnt.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            elmnt.innerHTML = "Page not found.";
-          }
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("include-html");
-          includeHTML();
-        }
-      };
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
-}
-includeHTML();
-
 // tooltips
 // TEMP SOLUTION
 
-setTimeout(function() {
-  $('[data-toggle="tooltip"]').tooltip();
-}, 1000);
+// setTimeout(function() {
+//   $('[data-toggle="tooltip"]').tooltip();
+// }, 1000);
